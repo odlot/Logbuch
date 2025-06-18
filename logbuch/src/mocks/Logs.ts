5bc5cba95ed58887bdb6740956237afe17071729
odlot <131015417+odlot@users.noreply.github.com>
2025-06-18 20:02:08 +0200

    Removes file extensions from mock

--- logbuch/src/mocks/Logs.ts
+++ logbuch/src/mocks/Logs.ts
@@ -3,7 +3,7 @@ import type { Log } from '../models/Log';
 export const Logs: Log[] = [
   {
     id: "92c957d7-1c35-410c-b7af-7bbf47d5b453",
-    filename: "2025-01-01-mock-001.json",
+    filename: "2025-01-01-mock-001",
     createdAt: "2025-01-01T00:00:00.000Z",
     notes: [{
       id: "ff652ffe-3a6c-4505-87c7-705dcadccf7f",
@@ -13,19 +13,19 @@ export const Logs: Log[] = [
   },
   {
     id: "e5bf790d-c7a3-4d4f-90b4-c5cc376e4938",
-    filename: "2025-01-02-mock-002.json",
+    filename: "2025-01-02-mock-002",
     createdAt: "2025-01-02T00:00:00.000Z",
     notes: []
   },
   {
     id: "ab471ce0-5e36-4b56-9659-9c3deef41212",
-    filename: "2025-01-03-mock-003.json",
+    filename: "2025-01-03-mock-003",
     createdAt: "2025-01-03T00:00:00.000Z",
     notes: []
   },
   {
     id: "25afab45-c297-4268-be18-3997b56af36e",
-    filename: "2025-01-03-mock-004.json",
+    filename: "2025-01-03-mock-004",
     createdAt: "2025-01-03T00:00:00.000Z",
     notes: []
   }
